3b1e9a5ddaaf9842633ee5390c7bc559e33cbfd8
iO <104570588+root9464@users.noreply.github.com>
2024-03-10 02:42:12 +0500

    Update database.go

--- database/database.go
+++ database/database.go
@@ -12,7 +12,7 @@ type Data struct {
 
 var Db Data
 func ConnectToDB() (Data, error) {
-	dsn := "postgres://postgres.fjfcexhrmlodechtmjxu:qwest7q8q1579@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres"
+	dsn := "блять вот сюда ссылку на бд"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("не удалось подключиться к базе данных")
